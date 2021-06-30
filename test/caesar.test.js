e02c0780fbe1e8cc130e92c225fce0083d23254e
Cat C <404@errur.com>
2021-06-30 12:21:36 -0500

    Added/refined test cases for caesar.js

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,27 +1,57 @@
 const { expect } = require("chai");
-const { caesar } = require("../src/caesar");
+const {caesar} = require('../src/caesar');
 
 
 describe("caesar", ()=>{
     it("should return false if shift equals 0 or is < -25 or > 25", ()=>{
-        const actual1 = caesar('testing', 0);
-        const actual2 = caesar('testing', 26);
-        const actual3 = caesar('testing', 29);
+        const tests = [];
 
-        const tests = [actual1, actual2, actual3];
-        tests.map((test)=>{
-            expect(test).to.be.false;
-        })
+        tests.push(caesar('testing', 0));
+        tests.push(caesar('testing', 26));
+        tests.push(caesar('testing', 29));
+
+        tests.map((test) => expect(test).to.be.false);
     })
 
-    it("should ignore non-alphabetic symbols", ()=>{
+
+    it("should ignore non-alphabetic symbols for input", ()=>{
         const testStrings = ["hello, world!", "404@errur.com", "this_is_a_test"];
-        
+        const ans = ["khoor, zruog!", "404@huuxu.frp", "wklv_lv_d_whvw"];
+        const ansKey = new Map();
+
+        if (testStrings.length === ans.length){
+            for (const i in testStrings){
+                ansKey.set(testStrings[i], ans[i]);
+            };
+        }else{
+            console.error("test string length differs from answer key length");
+        }
         testStrings.map((testStr)=>{
-            const actual = caesar(testStr, 3);
-            
-        })
+            let testAns = caesar(testStr, 3);            
+            expect(testAns).to.be.eql(ansKey.get(testStr));
+        });
 
     })
 
+
+    it("Should ignore capitalized letters for input", ()=>{
+        const testCase = 'ThInKfUl';
+        const testAns = caesar(testCase, 3);
+        expect(testAns).to.be.eql("wklqnixo");
+        expect(testAns).to.be.eql(testAns.toLowerCase());
+    })
+
+
+    it("should only process input if it is a string - if not, then return false", ()=>{
+        const testCases = [null, {key: 'value'}, 111];
+        testCases.map((test) => expect(caesar(test, 3)).to.be.false);
+    })
+
+
+    it("should handle non-numeric shift by returning false", ()=>{
+        const testCases = [null, {key: 'value'}, "111"];
+
+        testCases.map((test)=> expect(caesar('thinkful', test)).to.be.false);
+    });
 })
+
