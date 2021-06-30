4b41abaa2f4320ffb929d33129a79a377d40649a
Cat C <404@errur.com>
2021-06-30 13:44:13 -0500

    Started code for argument validity function

--- src/polybius.js
+++ src/polybius.js
@@ -4,16 +4,34 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  function polybius(input, encode = true) {
+  function polybius(input, encode=true) {
+    const argsValid = argumentValidity(input, encode);
+    console.log("argsValid: ", argsValid);
+
     const cipherMap = getCipherMap();
     const userInputArr = input.toLowerCase().split("");
-    if (encode) {
-      return encodeData(userInputArr, cipherMap);
-    } else {
-      return decodeData(userInputArr, cipherMap);
-    }
+
+    return encode ? encodeData(userInputArr, cipherMap) : decodeData(userInputArr, cipherMap);
   }
-  
+
+  function argumentValidity(input, encode){
+    const consensus = {};
+    const inputArr = input.split("");
+    if (!encode){
+      const numCheck = inputArr.map((input)=>isNaN(input));
+      
+      for (let n in numCheck){
+        if (numCheck[n] === false && numCheck[n+1] === false){
+          consensus.adjacentNums = true;
+          break;
+        }
+      }
+    }
+
+    return Object.keys(consensus).every((item)=>item === true);
+  }
+
+  // maps columns and rows to alphabet keys
   function getCipherMap() {
     const alphabet = getAlphabetArray();
     let polyMap = new Map();
@@ -21,10 +39,12 @@ const polybiusModule = (function () {
     let column = 1;
 
     for (const char of alphabet) {
-      let coord = { column, row };
-      polyMap.set(char, { column, row });
+      const coord = { column, row };
+      polyMap.set(char, coord);
+
+      // specifically for making sure 'j' is set to the same coord data as 'i'
       if (char === "i") {
-        polyMap.set(char, { column, row });
+        polyMap.set(char, coord);
         continue;
       }
       column < 5 ? column++ : (column = 1);
@@ -33,30 +53,71 @@ const polybiusModule = (function () {
 
     return polyMap;
   }
+  
+  // takes an array of letters and returns a string of translated numbers
+  function encodeData(dataArr, cipherMap) {
+    let encoded = "";
 
-  function getAlphabetArray(caps = false) {
-    // 65 is capitalized A; 97 is lowercase A
-    let asciiRep = caps ? 65 : 97;
-    let allLetters = [];
-
-    for (let n = 0; n < 26; n++) {
-      allLetters.push(String.fromCharCode(asciiRep));
-      asciiRep++;
-    }
-
-    return allLetters;
+    return dataArr.reduce((encoded, char)=>{
+      if (/[a-z]/.test(char)) {
+        const { column, row } = cipherMap.get(char);
+        encoded += column;
+        encoded += row;
+      }else{
+        encoded += char;
+      }
+      return encoded;
+    }, "");
   }
 
+  // takes an array of numbers and returns a string of translated letters
+  function decodeData(dataArr, cipherMap) {
+    if (!inputIsEven(dataArr)) return false;
+
+    let numPair = [];
+    const toDecode = [];
+
+    dataArr.map((num) => {
+      if (/[0-9]/.test(num)) {
+        if (numPair.length < 2) numPair.push(Number(num));
+        if (numPair.length === 2) {
+          toDecode.push(numPair);
+          numPair = [];
+        }
+      } else {
+        toDecode.push(num);
+      }
+    })
+
+    return toDecode.reduce((decodeStr, element) => {
+      if (Array.isArray(element)) {
+        let compareObj = { column: element[0], row: element[1] };
+        let char = getKeyByValue(compareObj, cipherMap);
+
+        if (!char) return false;
+        // accounts for 'j' and 'i' sharing the same coordinates and having a special output
+        if (char === "i" || char === "j") decodeStr += "(i/j)"
+        else decodeStr += char;
+      } else {
+        decodeStr += element;
+      }
+      return decodeStr;
+    }, "");
+  }
+
+  function getAlphabetArray(){
+    return "abcdefghijklmnopqrstuvwxyz".split("");
+  }
+  // find matching key by comparing value properties
   function getKeyByValue(value, mapObj) {
     for (const [k, v] of mapObj.entries())
       if (v.column === value.column && v.row === value.row) return k;
-
     return null;
   }
 
   // helper function for decodeData
   function inputIsEven(inputs) {
-    let qtyNums = inputs.reduce((nums, input) => {
+    const qtyNums = inputs.reduce((nums, input) => {
       if (/[0-9]/.test(input)) nums++;
       return nums;
     }, 0);
@@ -64,65 +125,10 @@ const polybiusModule = (function () {
     return qtyNums % 2 !== 0 ? false : true;
   }
 
-  // takes an array of letters and returns a string of translated numbers
-  function encodeData(dataArr, cipherMap) {
-    let encoded = "";
-    dataArr.map((char) => {
-      if (/[a-z]/.test(char)) {
-        let { column, row } = cipherMap.get(char);
-        encoded += column;
-        encoded += row;
-      } else {
-        encoded += char;
-      }
-    });
-
-    return encoded;
-  }
-
-  // takes an array of numbers and returns a string of translated letters
-  function decodeData(dataArr, cipherMap) {
-    let placeholder = [];
-    let toDecode = [];
-    let decodeStr = "";
-
-    if (!inputIsEven(dataArr)) return false;
-
-    dataArr.map((num) => {
-      if (/[0-9]/.test(num)) {
-        if (placeholder.length < 2) {
-          placeholder.push(Number(num));
-        }
-        if (placeholder.length === 2) {
-          toDecode.push(placeholder);
-          placeholder = [];
-        }
-      } else {
-        toDecode.push(num);
-      }
-    });
-    toDecode.map((element) => {
-      if (Array.isArray(element)) {
-        let compareObj = { column: element[0], row: element[1] };
-        let char = getKeyByValue(compareObj, cipherMap);
-
-        if (!char) return false;
-        if (char != "i") decodeStr += char;
-        // j and i share the same coordinates, so switch one out for the other if one of the chars exist
-        if (char === "i" && !decodeStr.includes("j")) decodeStr += "j";
-        if (char === "i" && decodeStr.includes("j")) decodeStr += char;
-      } else {
-        decodeStr += element;
-      }
-    });
-
-    return decodeStr;
-  }
-
-
   return {
     polybius,
   };
 })();
+
 
 module.exports = { polybius: polybiusModule.polybius };
